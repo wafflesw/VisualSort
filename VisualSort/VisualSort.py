9e13d3bf49f14363f3d4a9b6be8edfe31b582025
James Weeden <wafflesw98@yahoo.com>
2023-01-12 19:36:27 -0800

    Removed several of non fuctioning sorts and cleaning up main while also adding comment

--- VisualSort/VisualSort.py
+++ VisualSort/VisualSort.py
@@ -58,6 +58,9 @@ class TrackedArray():
 plt.rcParams["figure.figsize"] = (12,8)
 plt.rcParams["font.size"] = 16
 
+#needs to add a more aesthetically pleasing timer
+#shows in cmd window and only on bubble need to place it on others
+#also adding it to the graph GUI
 def tstart():
     t0 = time.perf_counter()
     return t0
@@ -65,6 +68,7 @@ def tstart():
 def tstop(t0):
     return time.perf_counter() - t0
 
+#this is used to display the different colored bars depending on get or set
 def update(frame, nums, container):
     for rectangle, height in zip(container.patches, nums.full_copies[frame]):
         rectangle.set_height(height)
@@ -88,41 +92,15 @@ def bubble_sort(nums):
 
 
 def insert_sort(nums):
-    for x in range(0,len(nums)-1):
-        if nums[x] > nums[x+1]:
-            temp = x
-            nums[x], nums[x+1] = nums[x+1], nums[x]
-            for j in range(x,-1,-1):
-                if nums[j] > nums[temp]:
-                    nums[j], nums[temp] = nums[temp], nums[j]
-                    temp = j
-
-def merge_sort(nums):
-    if len(nums) > 1:
-        mid = len(nums)//2
-        L = nums[mid:]
-        R = nums[:mid]
-        merge_sort(L)
-        merge_sort(R)
-        i = j = k = 0
-        while i < len(L) and j < len(R):
-            if L[i] >= R[j]:
-                nums[k] = R[j]
-                j += 1
-            else:
-                nums[k] = L[i]
-                i += 1
-            k += 1
-        while i < len(L):
-            nums[k] = L[i]
-            i += 1
-            k += 1
-        while j < len(R):
-            nums[k] = R[j]
-            j += 1
-            k += 1
-
-#for low and high use 0 and len of array - 1
+    for x in range(1,len(nums)):
+        key = nums[x]
+        j = x - 1
+        while j >= 0 and key < nums[j]:
+            nums[j+1] = nums[j]
+            j -= 1
+            nums[j+1] = key
+            
+#for low and high use 0 and len of list - 1
 def quick_sort(nums, low, high):
     if low < high:
         pivot = partition(nums, low, high)
@@ -141,71 +119,6 @@ def partition(nums, low, high):
     nums[i + 1], nums[high] = nums[high], nums[i+1]
     return i + 1
 
-
-def count_sort(nums):
-    max_element = int(max(nums))
-    min_element = int(min(nums))
-    range_of_elements = max_element - min_element + 1
-    count_nums = [0 for i in range( range_of_elements )]
-    output_nums = [0 for i in range( len(nums ))]
-    for i in range(0, len(nums)):
-        count_nums[nums[i]-min_element] += 1
-    for i in range(1, len(count_nums)):
-        count_nums[i] += count_nums[i-1]
-    for i in range(len(nums)-1,-1,-1):
-        output_nums[count_nums[nums[i] - min_element] - 1] = nums[i]
-        count_nums[nums[i] - min_element] -= 1
-    for i in range(0, len(nums)):
-        nums[i] = output_nums[i]
-    return nums
-
-def bucket_sort(nums, n):
-    min_ele = min(nums)
-    max_ele = max(nums)
-    rnge = (max_ele - min_ele)/n
-    bucket = []
-    for i in range(n):
-        bucket.append([])
-    for i in range(len(nums)):
-        dif = (nums[i] - min_ele)/ rnge - int((nums[i] - min_ele)/rnge)
-        if (dif == 0 and nums[i] != min_ele):
-            bucket[int((nums[i] - min_ele)/rnge) - 1].append(nums[i])
-        else:
-            bucket[int((nums[i] - min_ele)/rnge)].append(nums[i])
-    for i in range(len(bucket)):
-        if len(bucket[i]) != 0:
-            #uses the python integrated sort which is a timsort
-            bucket[i].sort() 
-    k = 0
-    for lst in bucket:
-        if lst:
-            for i in lst:
-                nums[k] = i
-                k = k + 1
-
-#this works by using the implemented counting sort and base 10 to compare each element 
-def radix_sort(nums):
-    max_ele = max(nums)
-    exp = 1
-    n = len(nums)
-    while max_ele / exp >= 1:
-        output = [0] * (n)
-        count = [0] * (100)
-        for i in range(0, n):
-            index = nums[i] // exp
-            count[index % 10] += 1
-        for i in range(1, 10):
-            count[i] += count[i - 1]
-        i = n - 1
-        while i >= 0:
-            index = nums[i] // exp
-            output[count[index % 10] - 1] = nums[i]
-            count[index % 10] -= 1
-            i -= 1
-        i = 0
-        for i in range(0, n):
-            nums[i] = output[i]
-        exp *=10
 
 def heap_sort(nums):
     n = len(nums)
@@ -233,7 +146,6 @@ def heapify(nums, n, x):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("generating numbers....")
-    #numList = np.random.randint(0,1000, 20)
     numList =np.round(np.linspace(0,1000,20),0)
     np.random.seed(0)
     np.random.shuffle(numList)
@@ -244,24 +156,15 @@ if __name__ == '__main__':
         numList = TrackedArray(numList,"full")
         option = input("What sort would you like to view(use option to view avaliable sorts or q to quit): ")
         if option == "option":
-            print("bubble(bubble sort), insert(insert sort), merge(merge sort), \n" 
-                  "quick(quick sort), count(count sort), bucket(bucket sort), \n" 
-                  "radix(radix sort), heap(heap sort)")
+            print("\n\nbubble(bubble sort), insert(insert sort), " 
+                  "quick(quick sort), heap(heap sort)\n\n")
             display = False
         elif option == "bubble":
             bubble_sort(numList)
         elif option == "insert":
             insert_sort(numList)
         elif option == "merge":
-           merge_sort(numList)
-        elif option == "quick":
             quick_sort(numList, 0, len(numList) -1)
-        elif option == "count":
-            count_sort(numList)
-        elif option == "bucket":
-            bucket_sort(numList,10)
-        elif option == "radix":
-            radix_sort(numList)
         elif option == "heap":
             heap_sort(numList)
         elif option == 'q':
@@ -278,13 +181,13 @@ if __name__ == '__main__':
             ani = FuncAnimation(fig, update, frames=range(len(numList.full_copies)), blit = True, interval = 1000/60, repeat = False, fargs=(numList, container))
             plt.show()
            
-            q = input("would you like to try again?(y or n): ")
-            if q == 'y':
-                numList = np.random.randint(0,1000, 20)
-                print("numbers randomized")
-                print(*numList)
-            else:
-                print("Thank you for using Vsort goodbye")
-                ans = False
+        q = input("would you like to try again?(y or n): ")
+        if q == 'y':
+            numList = np.random.randint(0,1000, 20)
+            print("numbers randomized")
+            print(*numList)
+        else:
+            print("Thank you for using Vsort goodbye")
+            ans = False
 
 
